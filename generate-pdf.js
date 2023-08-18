2d459fea8b6a61416e2918a56a9c389e02b67c09
stas <stas.sklyar.dev@gmail.com>
2023-08-18 05:15:52 -0700

    in progress

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,33 +1,40 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
+const fsPromises = require('fs').promises
 
 async function generatePDF(data) {
-    try {
-        const browser = await puppeteer.launch({
-            headless: false
-        })
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log("generatePDF")
+            const browser = await puppeteer.launch({
+                headless: false,
+                devtools: true
+            })
 
-        const page = await browser.newPage()
+            const page = await browser.newPage()
 
-        // fs.writeFileSync('template/data.json', JSON.stringify(data), 'utf8')
+            // fs.writeFileSync('template/data.json', JSON.stringify(data), 'utf8')
+            await fsPromises.writeFile('template/data.json', JSON.stringify(data), 'utf8');
 
-        const html = fs.readFileSync('template/index.html', 'utf-8')
-        await page.setContent(html, { waitUntil: 'load' })
+            const html = await fsPromises.readFile('template/index.html', 'utf-8')
+            await page.setContent(html, { waitUntil: 'load' })
+            await page.waitForTimeout(2000); // ждем 1 секунду,
 
-        await page.emulateMediaType('screen')
+            await page.emulateMediaType('screen')
 
-        const pdfBuffer = await page.pdf({
-            format: 'A4',
-            printBackground: true
-        })
+            const pdfBuffer = await page.pdf({
+                format: 'A4',
+                printBackground: true
+            })
 
-        // await browser.close()
+            await browser.close()
 
-        return { pdfBuffer, error: null }
-    } catch (error) {
-        console.error(error)
-        return { pdfBuffer: null, error }
-    }
+            resolve({ pdfBuffer, error: null })
+        } catch (error) {
+            console.error(error)
+            reject({ pdfBuffer: null, error })
+        }
+    })
 }
 
 module.exports = { generatePDF }
